39dd871aca9bcafa0695054d5c627553d6083481
Alberto Gald├ímez <alejandro.agaldameez@gmail.com}>
2019-11-20 23:13:05 -0600

    Enviando practica 10 By Alberto Gald├ímez

---
+++ GitBot.bat
@@ -0,0 +1,9 @@
+@echo off
+git init
+git add .
+git commit -m "Enviando practica 10 By Alberto Galdámez"
+git remote add origin https://github.com/unicaes-ing/practica-10-Beertz.git
+git push origin master
+git status
+pause
+exit
